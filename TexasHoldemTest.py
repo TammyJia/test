d2741f89ebeef042b3e585f2a8f52096134d140c
tammy <tammyjia@stu.xjtu.edu.cn>
2020-03-14 11:51:11 +0800

    unit test

--- TexasHoldemTest.py
+++ TexasHoldemTest.py
@@ -7,7 +7,7 @@ class TestTexasHoldem(unittest.TestCase):
         texasholdem = TexasHoldem("1S,5D,AH,3D,1C", "2C 3H 4S 8C AH")
         self.assertEqual(texasholdem.check_input(), False)
         del texasholdem
-        texasholdem = TexasHoldem("1S 5D AH 3D 1C", "2C 3H 4S 8C AH")
+        texasholdem = TexasHoldem("2H 3D 5S 9C KD", "2C 3H 4S 8C AH")
         self.assertEqual(texasholdem.check_input(), True)
         del texasholdem
         texasholdem = TexasHoldem("1S 5D AH 3D", "2C 3H 4S 8C AH")
@@ -15,6 +15,22 @@ class TestTexasHoldem(unittest.TestCase):
         del texasholdem
         texasholdem = TexasHoldem("1TTT 5D AH 3D 1C", "2C 3H 4S 8C AH")
         self.assertEqual(texasholdem.check_input(), False)
+        del texasholdem
+
+    def test_sortcards(self):
+        texasholdem = TexasHoldem("2H 3D 5S 9C KD", "2C 3H 4S 8C AH")
+        texasholdem.card_sort()
+        self.assertEqual(texasholdem.newblack, ['KD', '9C', '5S', '3D', '2H'])
+        self.assertEqual(texasholdem.newwhite, ['AH', '8C', '4S', '3H', '2C'])
+
+        texasholdem.newblack = ['7D', '9C', '7S', '3D', '8H']
+        texasholdem.newwhite = ['3H', '5D', '3S', '5C', '3D']
+        texasholdem.card_sort()
+        print(texasholdem.newblack)
+        print(texasholdem.newwhite)
+        self.assertEqual(texasholdem.newblack, ['7D', '7S', '9C', '8H', '3D'])
+        self.assertEqual(texasholdem.newwhite, ['3S', '3D', '3H', '5D', '5D'])
+
         del texasholdem
 
 
